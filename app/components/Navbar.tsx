662b6fd3d8d7adff31ec6707ac005033ced4c025
David <davidadanponce@outlook.com>
2025-02-05 16:52:57 -0500

    small

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link'; // Import Link from next/link
 const Navbar: React.FC = () => {
   return (
     <nav className="fixed top-0 left-0 w-full bg-transparent backdrop-blur-md z-50">
-      <div className="max-w-6xl mx-auto flex justify-center items-center px-8 py-4">
+      <div className="max-w-sm mx-auto flex justify-center items-center px-8 py-4">
         <div className="flex space-x-6 items-center">
           <Link href="/" className="text-white hover:text-blue-300 transition">HOME</Link>
           <Link href="/training" className="text-white hover:text-blue-300 transition">ONLINE PERSONAL TRAINING</Link>
