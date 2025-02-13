dba704234b09981a160c8c3cdc7f3b7b5a93ec50
David <davidadanponce@outlook.com>
2025-02-13 15:12:09 -0500

    ds3

--- app/components/Herosection.tsx
+++ app/components/Herosection.tsx
@@ -6,7 +6,7 @@ export default function HeroSection() {
   return (
     <div className="min-h-screen min-w-screen bg-transparent flex items-center justify-center"> {/* Centering parent container */}
       <div
-        className="lg:w-[80%] px-[5%] py-[2%] flex rounded-lg"
+        className="lg:w-[60%] px-[5%] py-[2%] flex rounded-lg"
         style={{ backgroundColor: 'rgba(169, 169, 169, 0.5)' }}
       >
         {/* Left Content */}
